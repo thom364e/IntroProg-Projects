e95e62e8bf8849a46f5f25fed1680ec2cb7c3c60
thom364e <thomas.jensen@hotmail.dk>
2024-02-29 17:38:51 +0100

    Adding code to the ExchangeEconomy.py file Copied code from the hints

--- inauguralproject/ExchangeEconomy.py
+++ inauguralproject/ExchangeEconomy.py
@@ -15,17 +15,36 @@ class ExchangeEconomyClass:
         par.w2A = 0.3
 
     def utility_A(self,x1A,x2A):
-        pass
+        ''' 
+        Implementing the utility function of agent A
+        Input: x1A and x2A being the consumption of good 1 and 2 for agent A
+        Output: Utility of agent A
+        '''
+        return x1A**self.par.alpha * x2A**(1 - self.par.beta)
 
     def utility_B(self,x1B,x2B):
-        pass
+        '''
+        Implementing the utility function of agent B
+        Input: x1B and x2B being the consumption of good 1 and 2 for agent B
+        Output: Utility of agent B
+        '''
+        return x1B**self.par.alpha * x2B**(1 - self.par.beta)
 
     def demand_A(self,p1):
-        pass
-
+        '''
+        Implementing the demand function of agent A obtained from solving the consumer problem
+        Input: p1 being the price of good 1 and the price of good 2 is used as numeraire
+        Output: x1A and x2A being the optimal consumption of good 1 and 2 for agent A given prices p1 and p2 = 1
+        '''
+        return self.par.alpha*(p1*self.par.w1A+self.par.w2A)/p1, (1 - self.par.alpha)*(p1*self.par.w1A+self.par.w2A)
+    
     def demand_B(self,p1):
-        pass
-
+        '''
+        Implementing the demand function of agent B obtained from solving the consumer problem
+        Input: p1 being the price of good 1 and the price of good 2 is used as numeraire
+        Output: x1A and x2A being the optimal consumption of good 1 and 2 for agent B given prices p1 and p2 = 1
+        '''
+        return self.par.beta*(p1*self.par.w1B+self.par.w2B)/p1, (1 - self.par.beta)*(p1*self.par.w1B+self.par.w2B)
     def check_market_clearing(self,p1):
 
         par = self.par
